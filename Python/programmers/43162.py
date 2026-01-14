04cb09a8aa5205fe81a4751788bc36c98c29ed0f
2021147542 <101931965+2021147542@users.noreply.github.com>
2026-01-14 16:28:11 +0900

    BFS 연습

--- Python/programmers/43162.py
+++ Python/programmers/43162.py
@@ -1,9 +1,22 @@
+from collections import deque
 def solution(n, computers):
     answer = 0
     visited = [False] * n
+    q = deque()
+    
     for i in range(n):
-        to_visit = [i]
-        while to_visit:
+        if visited[i]: continue
+        answer += 1
+        q.append(i)
+        visited[i] = True
+        while q:
+            point = q.popleft()
+            for j in range(n):
+                if computers[point][j] and not visited[j]:
+                    q.append(j)
+                    visited[j] = True
+        
             
 
     return answer
+
