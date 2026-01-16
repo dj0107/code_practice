32aa360a1d8f749d8948f3545ac5785635acfb66
2021147542 <101931965+2021147542@users.noreply.github.com>
2026-01-16 22:20:30 +0900

    임시

---
+++ Python/programmers/64063.py
@@ -0,0 +1,18 @@
+class rangeTree:
+
+    class Node:
+        def __init__(self ,start, end):
+            self.start = start
+            self.end = end
+            self.left = None
+            self.right = None
+            self.parent = None
+
+    def __init__(self):
+        pass
+
+
+def solution(k, room_number):
+    answer = []
+
+    return answer
