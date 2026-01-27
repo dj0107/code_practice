170c8dceb85c3981241b830a83c44a412071bc22
2021147542 <101931965+2021147542@users.noreply.github.com>
2026-01-27 17:24:12 +0900

    radix sort 구현 및 백준 문제풀이

--- Python/sort.py
+++ Python/sort.py
@@ -1,13 +1,16 @@
 import time
 import random
 import sys
+from collections import deque
 sys.setrecursionlimit(999999)
 INF = float('inf')
 
 def isSorted(arr):
     n = len(arr)
     for i in range(1, n):
-        if arr[i] < arr[i-1]: return False
+        if arr[i] < arr[i-1]: 
+            print(arr[i-1], "in front of", arr[i])
+            return False
     return True
 
 def insertionSort(arr):
@@ -52,8 +55,24 @@ def merge(arr1, arr2, arr):
             arr[left + right] = arr1[left]
             left += 1
     
-def quickSort(arr):
-    pass
+def radixSort(arr, d):
+    """
+    자릿수 d인 정수에 대해 radix sort.
+    """
+    
+    for i in range(d):
+        darr = [deque() for _ in range(10)]
+        to_divide = 10 ** i
+        for j in range(len(arr)):
+            idx = (arr[j] % (10 * to_divide)) // to_divide
+            darr[idx].append(arr[j])
+        arr.clear()
+        for j in range(10):
+            while darr[j]:
+                arr.append(darr[j].popleft())
+        
+        
+
 
 # def infLoop(n):
 #     print(n)
@@ -69,26 +88,40 @@ if __name__ == '__main__':
     arr1 = arr1[:10000]
     arr2 = arr1[:]
     arr3 = arr1[:]
+    arr4 = arr1[:]
+    d = 0
+    for val in arr1:
+        d = max(d, val)
+    print(f"biggest one: {d}")
+    d = len(str(d))
 
-    # insertion sort
-    start = time.time()
-    print(isSorted(arr1))
-    insertionSort(arr1)
-    print(isSorted(arr1))
-    print("elapsed time:", time.time() - start)
+    # # insertion sort
+    # start = time.time()
+    # print(isSorted(arr1))
+    # insertionSort(arr1)
+    # print(isSorted(arr1))
+    # print("elapsed time:", time.time() - start)
 
-    # selection sort
-    start = time.time()
-    print(isSorted(arr2))
-    selectionSort(arr2)
-    print(isSorted(arr2))
-    print("elapsed time:", time.time() - start)
+    # # selection sort
+    # start = time.time()
+    # print(isSorted(arr2))
+    # selectionSort(arr2)
+    # print(isSorted(arr2))
+    # print("elapsed time:", time.time() - start)
 
     # merge sort
     start = time.time()
     print(isSorted(arr3))
     mergeSort(arr3)
     print(isSorted(arr3))
+    print("elapsed time:", time.time() - start)
+
+    # radix sort
+    start = time.time()
+    print(isSorted(arr4))
+    radixSort(arr4, d)
+    print(isSorted(arr4))
+    # print(arr4)
     print("elapsed time:", time.time() - start)
 
 
