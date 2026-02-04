21280f5f9ebce915ac42f8d4aa60dab58a9d9474
2021147542 <101931965+2021147542@users.noreply.github.com>
2026-02-04 16:48:19 +0900

    배낭 문제

--- Python/baekjoon/12865.py
+++ Python/baekjoon/12865.py
@@ -1,21 +1,18 @@
 import sys
 input = sys.stdin.readline
+INF = float('inf')
 n, k = map(int, input().split())
 things = []
 for i in range(n):
     w, v = map(int, input().split())
-    things.append([w, v])
+    things.append([w, v]) # [무게, 가치]
 
-things.sort() # 이게 필요할까 굳이?
-dp = [[0, 0]] * n # 지금은 0 이지만 나중에 (총무게, 총가치)로 바뀜
-dp[0] = [things[0][0], things[0][1]] if things[0][0] <= k else [0, 0]
-for i in range(1, n):
-    for j in range(i):
-        if dp[j][0] + things[i][0] <= k and (dp[i][1] < dp[j][1] + things[i][1] or (dp[j][0] + things[i][0] < dp[i][1] and dp[i][1] == dp[j][1] + things[i][1])):
-            dp[i][1] = dp[j][1] + things[i][1]
-            dp[i][0] = dp[j][0] + things[i][0]
-ans = 0
-for i in range(n):
-    ans = max(ans, dp[i][1])
-print(ans)
-            
+dp = [[0] * (k+1) for _ in range(n+1)] # 개수 x 무게
+for i in range(1, n+1):
+    for j in range(1, k+1):
+        if things[i-1][0] > j: # 지금 추가로 고려할 수 있게된 물건이 현재의 총가용무게를 넘어서 고려할 필요도 없는 경우, 이전 경우와 동일
+            dp[i][j] = dp[i-1][j]
+        else:
+            dp[i][j] = max(dp[i-1][j], dp[i-1][j-things[i-1][0]] + things[i-1][1])
+
+print(dp[n][k])
